6091f3866a1ae1ad36451d97a75834a1164682cf
EssKayz <esskay@protonmail.com>
2019-09-19 21:38:18 +0300

    Heroku postgresql
    Added requirements and stuff to make heroku use postgresql

--- src/__init__.py
+++ src/__init__.py
@@ -4,8 +4,14 @@ app = Flask(__name__)
 
 #Import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
-app.config["SQLALCHEMY_ECHO"] = True
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+    app.config["SQLALCHEMY_ECHO"] = True
+    
 db = SQLAlchemy(app)
 
 #Import views
