fadcdefe8350c3a940446a0dd026c0f21c284295
EssKayz <esskay@protonmail.com>
2019-10-03 15:24:02 +0300

    Revert "Update __init__.py"
    This reverts commit 2278266ea79496adbf292802fd483af9fef136b1.

--- src/__init__.py
+++ src/__init__.py
@@ -1,46 +1,43 @@
-from flask_login import LoginManager
-from os import urandom
-from src.auth.models import User
-from src.auth import views
-from src.auth import models
-from src.manufacturers.products import models
-from src.manufacturers import models
-from src.manufacturers.products import views
-from src.manufacturers import views
-from src import views
-import os
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 from flask import Flask
 app = Flask(__name__)
 
-bcrypt = Bcrypt(app)
 
-# Import sqlalchemy
+#Import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 if os.environ.get("HEROKU"):
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 else:
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
     app.config["SQLALCHEMY_ECHO"] = True
-
+    
 db = SQLAlchemy(app)
 
-# Import views
+#Import views
+from src import views
+from src.manufacturers import views
+from src.manufacturers.products import views
 
-# Import database models
+#Import database models
+from src.manufacturers import models
+from src.manufacturers.products import models
 
-# Import user auth
+#Import user auth
+from src.auth import models
+from src.auth import views
 
-# Logins
+#Logins
+from src.auth.models import User
+from os import urandom
 app.config["SECRET_KEY"] = urandom(32)
-
+ 
+from flask_login import LoginManager
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 login_manager.login_view = "auth_login"
 login_manager.login_message = "Please login to use this functionality."
-
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -48,3 +45,4 @@ def load_user(user_id):
 
 
 db.create_all()
+
